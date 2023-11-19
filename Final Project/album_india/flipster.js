e3cabf153ca3b33c6ee04fca36f0aa4e2b856ba9
karan812060 <145696663+karan812060@users.noreply.github.com>
2023-11-18 18:37:29 -0800

    Delete Final Project directory

--- Final Project/album_india/flipster.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $("#carousel").flipster({
-            style: 'carousel',
-            spacing: -0.5,
-            nav: true,
-            buttons:   true,
-       });
-    });
